889a38b192f54537657bfdf3b610cd189bd056e0
prince king <wyhddzxx2@gmail.com>
2016-12-18 12:44:08 +0800

    add list_repo_url function

--- dl.sh
+++ dl.sh
@@ -6,32 +6,36 @@
 
 #set -e
 #set -o pipefail
-for url in `cat repo_url/gnome_repo_url.txt`
+source list_repo_url.sh
+for repo_url_file in `list_repo_url $1`
 do
-	dir=`echo $url|cut -d \/ -f 5`
-	if [ ! -d $dir.git ]
-	then
-		echo -e "\033[32mCLONE $dir.git\033[0m"
-		echo -e "\033[32m------------------------------------------------\033[0m"
-		git clone --mirror $url
-	else
-		echo -e "\033[32mUPDATE $dir.git\033[0m"
-		echo -e "\033[32m------------------------------------------------\033[0m"
-		cd $dir.git
-		git remote -v update 2>/dev/null
-		if [ $? != 0 ]
+	for url in `cat $repo_url_file`
+	do
+		dir=`echo $url|cut -d \/ -f 5`
+		if [ ! -d $dir.git ]
 		then
-#			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m"
-			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m" >&1 |tee -a ../sync_err.log
+			echo -e "\033[32mCLONE $dir.git\033[0m"
+			echo -e "\033[32m------------------------------------------------\033[0m"
+			git clone --mirror $url
 		else
-			countObject=`ls ./objects/pack|wc -l`
-			if (($countObject > 3))
+			echo -e "\033[32mUPDATE $dir.git\033[0m"
+			echo -e "\033[32m------------------------------------------------\033[0m"
+			cd $dir.git
+			git remote -v update 2>/dev/null
+			if [ $? != 0 ]
 			then
-				git repack -a -b -d
+	#			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m"
+				echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m" >&1 |tee -a ../sync_err.log
+			else
+				countObject=`ls ./objects/pack|wc -l`
+				if (($countObject > 3))
+				then
+					git repack -a -b -d
+				fi
 			fi
+			cd ..
 		fi
-		cd ..
-	fi
-	echo -e "\033[32mDONE\033[0m"
+		echo -e "\033[32mDONE\033[0m"
+	done
 done
 exit 0
