40b5332cbf4c58f32dd93955f7113ee1200e6daa
prince king <wyhddzxx2@gmail.com>
2016-12-18 10:45:31 +0800

    add error log function

--- dl.sh
+++ dl.sh
@@ -6,14 +6,18 @@ do
 	dir=`echo $line|cut -d \/ -f 5`
 	if [ ! -d $dir.git ]
 	then
+		echo -e "\033[32mCLONE $dir.git\033[0m"
+		echo -e "\033[32m------------------------------------------------\033[0m"
 		git clone --mirror $line
 	else
+		echo -e "\033[32mUPDATE $dir.git\033[0m"
+		echo -e "\033[32m------------------------------------------------\033[0m"
 		cd $dir.git
 		git remote -v update 2>/dev/null
 		if [ $? != 0 ]
 		then
-			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m"
-			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m" > ../sync.log
+#			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m"
+			echo -e "\033[31merror:\t`pwd`\tmaybe is not a normal git bare repository\033[0m" >&1 |tee -a ../sync_err.log
 		else
 			countObject=`ls ./objects/pack|wc -l`
 			if (($countObject > 3))
@@ -23,5 +27,6 @@ do
 		fi
 		cd ..
 	fi
+	echo -e "\033[32mDONE\033[0m"
 done
 exit 0
