c9a7515fe03c4bff870e5b6305d37d6a73b49272
prince king <wyhddzxx2@gmail.com>
2016-12-30 15:46:37 +0800

    enhance list_repo_url function ,fix some bugs

--- list_repo_url.sh
+++ list_repo_url.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#Copyright (c) 2016 Prince King <wyhddzxx2@gmail.com>
+#This file is under MIT license
+
 function list_repo_url(){
-	for line in $(ls $1/*_repo_url.txt)
+	if [ $# == 0 ]
+	then
+#		echo "Usage: list_repo_url directory1 directory2 .. directory[n]"
+		return 1
+	fi
+	local url_dirs="$@"
+	local url_dir
+	for url_dir in ${url_dirs[@]}
 	do
-		echo $line
+		for line in $(ls $url_dir/*_git_repo_url.txt 2>/dev/null)
+		do
+			echo $line
+		done
 	done
 	return 0
 }
---
+++ test3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source list_repo_url.sh
+list_repo_url repo_url repo_url2 repo_url3
